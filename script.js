5ebb2a4c952aeab26bc542caa152603cdb27b336
Edi <128224911+edijrdev@users.noreply.github.com>
2023-04-08 12:52:53 -0300

    Add function to change grid items' color

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
+    gridItem.addEventListener("mouseover", changeColor);
+    gridItem.addEventListener("mousedown", changeColor);
     container.appendChild(gridItem);
   }
 }
 
+function changeColor(e) {
+  if(e.type === "mouseover" && mouseIsDown === false) return;
+  e.target.style.backgroundColor = "black";
+}
+
 const container = document.querySelector("#container");
+let mouseIsDown = false;
+window.addEventListener("mousedown", () => mouseIsDown = true);
+window.addEventListener("mouseup", () => mouseIsDown = false);
 window.onload = createGrid(16);
