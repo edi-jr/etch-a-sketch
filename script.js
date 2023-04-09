22e00882543adc255d9d68535d9c0ef71a4e0fa7
Edi <128224911+edijrdev@users.noreply.github.com>
2023-04-09 07:20:18 -0300

    Add color picker

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createGrid() {
-  const gridSize = input.value;
+  const gridSize = sizeInput.value;
   container.textContent = "";
-  inputLabel.textContent = `Grid size: ${gridSize} x ${gridSize}`;
+  sizeLabel.textContent = `Grid size: ${gridSize} x ${gridSize}`;
   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for (let i = 0; i < gridSize * gridSize; i++) {
@@ -13,16 +13,25 @@ function createGrid() {
   }
 }
 
+function setPenColor() {
+  penColor = colorInput.value;
+}
+
 function changeColor(e) {
   if(e.type === "mouseover" && mouseIsDown === false) return;
-  e.target.style.backgroundColor = "black";
+  e.target.style.backgroundColor = penColor;
 }
 
 const container = document.querySelector("#container");
-const input = document.querySelector("input");
-const inputLabel = document.querySelector("#input-label");
+const sizeInput = document.querySelector("#grid-size");
+const sizeLabel = document.querySelector("#size-label");
+const colorInput = document.querySelector("#pen-color");
+
 let mouseIsDown = false;
-input.addEventListener("input", createGrid);
+let penColor = "#202020";
+
+sizeInput.addEventListener("input", createGrid);
+colorInput.addEventListener("input", setPenColor);
 window.addEventListener("mousedown", () => mouseIsDown = true);
 window.addEventListener("mouseup", () => mouseIsDown = false);
 window.onload = createGrid();
