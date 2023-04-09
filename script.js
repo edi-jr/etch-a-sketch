757e77adf075416b2c220510ee8ac766186374fa
Edi <128224911+edijrdev@users.noreply.github.com>
2023-04-09 07:49:41 -0300

    Add rainbow mode

--- script.js
+++ script.js
@@ -14,24 +14,34 @@ function createGrid() {
 }
 
 function setPenColor() {
-  penColor = colorInput.value;
+  if(rainbowButton.classList.contains("active")) {
+    const randomR = Math.round(Math.random() * 256);
+    const randomG = Math.round(Math.random() * 256);
+    const randomB = Math.round(Math.random() * 256);
+    penColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else {
+    penColor = colorInput.value;
+  }
+  return penColor;
 }
 
 function changeColor(e) {
   if(e.type === "mouseover" && mouseIsDown === false) return;
-  e.target.style.backgroundColor = penColor;
+  e.target.style.backgroundColor = setPenColor();
 }
 
 const container = document.querySelector("#container");
 const sizeInput = document.querySelector("#grid-size");
 const sizeLabel = document.querySelector("#size-label");
 const colorInput = document.querySelector("#pen-color");
+const rainbowButton = document.querySelector("#rainbow");
 
 let mouseIsDown = false;
 let penColor = "#202020";
 
 sizeInput.addEventListener("input", createGrid);
 colorInput.addEventListener("input", setPenColor);
+rainbowButton.addEventListener("click", (e) => e.target.classList.toggle("active"));
 window.addEventListener("mousedown", () => mouseIsDown = true);
 window.addEventListener("mouseup", () => mouseIsDown = false);
 window.onload = createGrid();
